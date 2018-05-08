22a170889cb892b3034d0c7e7f1d519266a20c7d
daniel <dg.melo@uniandes.edu.co>
2018-05-08 08:45:38 -0500

    final

--- proba.py
+++ proba.py
@@ -6,18 +6,41 @@ M = 1.989e30
 
 G = 6.674e-11
 
-def proba( v, r, alfa):
+UA = 149597870700.0
 
-    cuerpo = v*v / (G*M)
-    return cuerpo**(1.0 / (1-alfa))
+def gauss(v, r, alfa):
 
+    sigma = 20000e20
+    r = r**(1-alfa)
+    arriba = (G*M - v*v/r)*(G*M - v*v/r)/(2.0*sigma*sigma)
+    
 
+    return np.exp(-arriba)
+
+    
+
+graf = np.linspace(0.0, 10, 100)
 
 
 pos = np.array([0.39, 1.0, 5.2], float)
-per = np.array([1.0/(0.24*365*24*3600), 1.0/(1.0*365*24*3600), 1.0/(11.86*365*24*3600)], float)
+per = np.array([1.0/(0.24*365.0*24.0*3600.0), 1.0/(1.0*365.0*24.0*3600.0), 1.0/(11.86*365.0*24.0*3600.0)], float)
+
+pos = UA*pos
 
 vel = per*2.0*np.pi*pos
 
+v1 = 2.0*np.pi*per[0]*pos[0]
 
-for i in range
+p = np.ones(len(graf))
+
+
+
+
+for i in range(len(graf)):
+        
+    p[i] = p[i] * gauss(v1, pos[0], graf[i])
+    
+
+plt.plot(graf, p)
+plt.show()
+
